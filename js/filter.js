8071482b2776b32a8397bafd83951efc07bda539
Irina Lysenko <65309323+Liri-39@users.noreply.github.com>
2020-11-08 23:20:14 +0200

    задание 22 (#13)

--- js/filter.js
+++ js/filter.js
@@ -1,67 +1,65 @@
 'use strict';
 
-(function () {
-  const filters = document.querySelector(`.map__filters`);
+const filters = document.querySelector(`.map__filters`);
+const MAX_SIMILAR_PINS_COUNT = 5;
 
-  const filteredDataArray = (dataArray) => {
+const filteredDataArray = (dataArray) => {
 
-    const filterValueArray = {
-      type: filters.querySelector(`#housing-type`).value,
-      price: filters.querySelector(`#housing-price`).value,
-      rooms: filters.querySelector(`#housing-rooms`).value,
-      guests: filters.querySelector(`#housing-guests`).value,
-      features: Array.from(filters.querySelectorAll(`.map__checkbox:checked`)).map((cb) => cb.value),
-    };
-    const FilterOfferPrice = {
-      priceMin: 10000,
-      priceMax: 50000
-    };
+  const filterValueArray = {
+    type: filters.querySelector(`#housing-type`).value,
+    price: filters.querySelector(`#housing-price`).value,
+    rooms: filters.querySelector(`#housing-rooms`).value,
+    guests: filters.querySelector(`#housing-guests`).value,
+    features: Array.from(filters.querySelectorAll(`.map__checkbox:checked`)).map((cb) => cb.value),
+  };
+  const FilterOfferPrice = {
+    priceMin: 10000,
+    priceMax: 50000
+  };
 
-    let array = [];
+  let array = [];
 
-    for (let i = 0; i < dataArray.length; i++) {
+  for (let i = 0; i < dataArray.length; i++) {
 
-      let isFiltered = true;
-      switch (isFiltered) {
-        case (dataArray[i].offer.type !== filterValueArray.type && filterValueArray.type !== `any`):
-          isFiltered = false;
-          break;
-        case (dataArray[i].offer.rooms !== Number(filterValueArray.rooms) && filterValueArray.rooms !== `any`):
-          isFiltered = false;
-          break;
-        case (dataArray[i].offer.guests !== Number(filterValueArray.guests) && filterValueArray.guests !== `any`):
-          isFiltered = false;
-          break;
-        case (dataArray[i].offer.price >= FilterOfferPrice.priceMin && filterValueArray.price === `low`):
-          isFiltered = false;
-          break;
-        case (dataArray[i].offer.price <= FilterOfferPrice.priceMax && filterValueArray.price === `high`):
-          isFiltered = false;
-          break;
-        case ((dataArray[i].offer.price <= FilterOfferPrice.priceMin || dataArray[i].offer.price >= FilterOfferPrice.priceMax) && filterValueArray.price === `middle`):
-          isFiltered = false;
-          break;
-
-        case (dataArray[i].offer.features !== filterValueArray.features && filterValueArray.features.length !== 0):
-          filterValueArray.features.forEach((feature) => {
-            if (!dataArray[i].offer.features.includes(feature)) {
-              isFiltered = false;
-            }
-          });
-          break;
-      }
-      if (isFiltered === true) {
-        array.push(dataArray[i]);
-      }
-      if (array.length === 5) {
+    let isFiltered = true;
+    switch (isFiltered) {
+      case (dataArray[i].offer.type !== filterValueArray.type && filterValueArray.type !== `any`):
+        isFiltered = false;
         break;
-      }
+      case (dataArray[i].offer.rooms !== Number(filterValueArray.rooms) && filterValueArray.rooms !== `any`):
+        isFiltered = false;
+        break;
+      case (dataArray[i].offer.guests !== Number(filterValueArray.guests) && filterValueArray.guests !== `any`):
+        isFiltered = false;
+        break;
+      case (dataArray[i].offer.price >= FilterOfferPrice.priceMin && filterValueArray.price === `low`):
+        isFiltered = false;
+        break;
+      case (dataArray[i].offer.price <= FilterOfferPrice.priceMax && filterValueArray.price === `high`):
+        isFiltered = false;
+        break;
+      case ((dataArray[i].offer.price < FilterOfferPrice.priceMin || dataArray[i].offer.price > FilterOfferPrice.priceMax) && filterValueArray.price === `middle`):
+        isFiltered = false;
+        break;
+
+      case (dataArray[i].offer.features !== filterValueArray.features && filterValueArray.features.length !== 0):
+        filterValueArray.features.forEach((feature) => {
+          if (!dataArray[i].offer.features.includes(feature)) {
+            isFiltered = false;
+          }
+        });
+        break;
     }
+    if (isFiltered === true) {
+      array.push(dataArray[i]);
+    }
+    if (array.length === MAX_SIMILAR_PINS_COUNT) {
+      break;
+    }
+  }
 
-    return array;
-  };
-  window.filter = {
-    filteredDataArray
-  };
-
-})();
+  return array;
+};
+window.filter = {
+  filteredDataArray
+};
--- js/move.js
+++ js/move.js
@@ -1,84 +1,83 @@
 'use strict';
-(function () {
-  const form = document.querySelector(`.ad-form`);
-  const coordLimits = {
-    minY: 0,
-    minX: 130,
-    maxX: 630
+
+const form = document.querySelector(`.ad-form`);
+const coordLimits = {
+  minY: 0,
+  minX: 130,
+  maxX: 630
+};
+const fillAddressValue = function (x, y, isDragged) {
+  let xOffset = mainPin.offsetHeight / 2;
+  if (isDragged) {
+    xOffset = mainPin.offsetHeight;
+  }
+  form.querySelector(`#address`).value = `${Math.round(y + mainPin.offsetWidth / 2)}, ${Math.round(x + xOffset)}`;
+};
+const map = document.querySelector(`.map`);
+const mainPin = map.querySelector(`.map__pin--main`);
+mainPin.addEventListener(`mousedown`, function (evt) {
+  evt.preventDefault();
+
+  let startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
   };
-  const fillAddressValue = function (x, y, isDragged) {
-    let xOffset = mainPin.offsetHeight / 2;
-    if (isDragged) {
-      xOffset = mainPin.offsetHeight;
+
+  let dragged = false;
+
+  const mouseMoveHandler = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    dragged = true;
+
+    let shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    let coordY = mainPin.offsetLeft - shift.x;
+    let coordX = mainPin.offsetTop - shift.y;
+
+    if (coordY < coordLimits.minY) {
+      coordY = coordLimits.minY;
+    } else if (coordY > map.offsetWidth - mainPin.offsetWidth) {
+      coordY = map.offsetWidth - mainPin.offsetWidth;
     }
-    form.querySelector(`#address`).value = `${Math.round(y + mainPin.offsetWidth / 2)}, ${Math.round(x + xOffset)}`;
+
+    if (coordX < coordLimits.minX) {
+      coordX = coordLimits.minX;
+    } else if (coordX > coordLimits.maxX) {
+      coordX = coordLimits.maxX;
+    }
+
+    mainPin.style.left = coordY + `px`;
+    mainPin.style.top = coordX + `px`;
+
+    fillAddressValue(coordX, coordY, true);
   };
-  const map = document.querySelector(`.map`);
-  const mainPin = map.querySelector(`.map__pin--main`);
-  mainPin.addEventListener(`mousedown`, function (evt) {
-    evt.preventDefault();
 
-    let startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
+  const mouseUpHandler = function (upEvt) {
+    upEvt.preventDefault();
 
-    let dragged = false;
+    document.removeEventListener(`mousemove`, mouseMoveHandler);
+    document.removeEventListener(`mouseup`, mouseUpHandler);
 
-    const mouseMoveHandler = function (moveEvt) {
-      moveEvt.preventDefault();
-
-      dragged = true;
-
-      let shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
+    if (dragged) {
+      const onClickPreventDefault = function (clickEvt) {
+        clickEvt.preventDefault();
+        mainPin.removeEventListener(`click`, onClickPreventDefault);
       };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      let coordY = mainPin.offsetLeft - shift.x;
-      let coordX = mainPin.offsetTop - shift.y;
-
-      if (coordY < coordLimits.minY) {
-        coordY = coordLimits.minY;
-      } else if (coordY > map.offsetWidth - mainPin.offsetWidth) {
-        coordY = map.offsetWidth - mainPin.offsetWidth;
-      }
-
-      if (coordX < coordLimits.minX) {
-        coordX = coordLimits.minX;
-      } else if (coordX > coordLimits.maxX) {
-        coordX = coordLimits.maxX;
-      }
-
-      mainPin.style.left = coordY + `px`;
-      mainPin.style.top = coordX + `px`;
-
-      fillAddressValue(coordX, coordY, true);
-    };
-
-    const mouseUpHandler = function (upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener(`mousemove`, mouseMoveHandler);
-      document.removeEventListener(`mouseup`, mouseUpHandler);
-
-      if (dragged) {
-        const onClickPreventDefault = function (clickEvt) {
-          clickEvt.preventDefault();
-          mainPin.removeEventListener(`click`, onClickPreventDefault);
-        };
-        mainPin.addEventListener(`click`, onClickPreventDefault);
-      }
-    };
-    document.addEventListener(`mousemove`, mouseMoveHandler);
-    document.addEventListener(`mouseup`, mouseUpHandler);
-  });
-  window.move = {
-    fillAddressValue
+      mainPin.addEventListener(`click`, onClickPreventDefault);
+    }
   };
-})();
+  document.addEventListener(`mousemove`, mouseMoveHandler);
+  document.addEventListener(`mouseup`, mouseUpHandler);
+});
+window.move = {
+  fillAddressValue
+};
